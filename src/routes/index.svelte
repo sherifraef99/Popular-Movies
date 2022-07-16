b074ab5c93374ed9ca18153deebd39d605974012
sherifraef99 <36931459+sherifraef99@users.noreply.github.com>
2022-07-16 17:23:13 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=f61dbf7a355c018a6762a87317efad25&language=en-US&page=1`
+		);
+		const data = await res.json();
+		console.log(data);
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+	export let popular;
+</script>
+
+<section>
+	<PopularMovies {popular} />
+</section>
---
+++ src/routes/movie/[id].svelte
@@ -0,0 +1,68 @@
+<script context="module">
+	export async function load({ fetch, params }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/${params.id}?api_key=f61dbf7a355c018a6762a87317efad25&language=en-US`
+		);
+		const movieDetail = await res.json();
+		if (res.ok) {
+			return {
+				props: { movieDetail }
+			};
+		}
+	}
+</script>
+
+<script>
+	export let movieDetail;
+</script>
+
+<div class="movie-details">
+	<div class="img-container">
+		<img
+			src={'https://image.tmdb.org/t/p/original' + movieDetail.backdrop_path}
+			alt={movieDetail.title}
+		/>
+	</div>
+	<div class="txt-container">
+		<h1>{movieDetail.title}</h1>
+		<p class="overview">
+			{movieDetail.overview}
+		</p>
+		<p>
+			<span>Release Date:</span>
+			{movieDetail.release_date}
+			<br />
+			<span>Budget:</span>
+			{movieDetail.budget}$
+			<br />
+			<span>Rating:</span>
+			{movieDetail.vote_average}
+			<br />
+			<span>Runtime:</span>
+			{movieDetail.runtime} Minutes
+			<br />
+		</p>
+	</div>
+</div>
+
+<style>
+	h1 {
+		padding: 1rem 0rem 2rem;
+	}
+	p {
+		padding: 1rem 0rem;
+	}
+	.img-container {
+		width: 100%;
+	}
+	img {
+		width: 100%;
+		border-radius: 1rem;
+	}
+	.movie-details {
+		margin: 2rem 20%;
+	}
+	span {
+		font-weight: bold;
+	}
+</style>
